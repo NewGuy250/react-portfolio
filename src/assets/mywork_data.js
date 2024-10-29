ce83f3b457db8a8464abb3508356ef07f0e49921
NewGuy250 <ndaniel220@yahoo.com>
2024-10-29 00:24:18 -0400

    Swapped out links

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -10,38 +10,37 @@ const mywork_data = [
     w_no: 1,
     w_name: "Web design",
     w_img: project1_img,
-    w_link: "https://newguy250.github.io/react-fitness-app/",
+    w_link: "https://newguy250.github.io/react-form/",
   },
   {
     w_no: 2,
     w_name: "Web design",
     w_img: project2_img,
-    w_link: "https://newguy250.github.io/to-do-list/",
+    w_link: "https://newguy250.github.io/react-weather/",
   },
   {
     w_no: 3,
     w_name: "Web design",
     w_img: project3_img,
-    w_link: "https://newguy250.github.io/Article-Preview/",
+    w_link: "https://newguy250.github.io/react-qr-code/",
   },
   {
     w_no: 4,
     w_name: "Web design",
     w_img: project4_img,
-    w_link: "https://newguy250.github.io/Frontend-Social-Proof/",
+    w_link: "https://newguy250.github.io/react-dice/",
   },
   {
     w_no: 5,
     w_name: "Web design",
     w_img: project5_img,
-    w_link:
-      "https://newguy250.github.io/Frontend-Interactive-Rating-Component/",
+    w_link: "https://newguy250.github.io/react-fitness-app/",
   },
   {
     w_no: 6,
     w_name: "Web design",
     w_img: project6_img,
-    w_link: "https://newguy250.github.io/Frontend-FAQ/",
+    w_link: "https://newguy250.github.io/to-do-list/",
   },
 ];
 
